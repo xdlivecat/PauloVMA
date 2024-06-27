5f51a94a725bec08e5f533600f0c481aab1f2989
xdlivecat <xdliverblx@hotmail.com>
2024-06-27 19:48:15 +0200

    Implemented webView functionality

--- app/src/main/java/com/xdliverblx/embotic/MainActivity.kt
+++ app/src/main/java/com/xdliverblx/embotic/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.xdliverblx.embotic
 
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.setInitialScale(1)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.isHorizontalScrollBarEnabled = false
+        webView.isClickable = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                url: String): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
         }
+        webView.loadUrl("https://dash.embotic.xyz")
     }
+}
