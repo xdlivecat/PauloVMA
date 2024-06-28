53a37939cc0858e3e327fbd24ff206c35e6c58fe
xdlivecat <xdliverblx@hotmail.com>
2024-06-28 10:32:03 +0200

    Update JVM Args

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 3
-        versionName = "1.1a"
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
